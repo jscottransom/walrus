9038e3968c1c6ad9cf9064fd01e1e342fd773cdd
jscottransom <james.scottran@gmail.com>
2025-05-12 11:08:28 -0700

    feat: Add index read ability

--- src/log/index.rs
+++ src/log/index.rs
@@ -30,9 +30,37 @@ impl Index {
 
     fn close(&mut self) -> Result<()> {
         self.file.sync_all()?;
-        self.file.set_len((self.size as i64).try_into().unwrap())?;
+        self.file.set_len(self.size);
 
         Ok(())
+
+    }
+
+    fn read(&self, offset: i64) -> Result<(u32, u64)> {
+        
+    
+        if self.size == 0 {
+            return Ok((0 as u32, 0 as u64));
+        } 
+        
+        let index: u32 = if offset == -1 {
+            (self.size as u32 / ENT_WIDTH as u32) - 1
+        } else {
+            offset as u32
+        };
+
+        let position = (index as u64) * ENT_WIDTH;
+        if self.size < position + ENT_WIDTH {
+            return Ok((0 as u32, 0 as u64));
+        } 
+          
+        let out_slice: [u8; 4] = self.mmap[position as usize..(position + OFF_WIDTH) as usize].try_into().expect("Expected 4 bytes");
+        let out = u32::from_be_bytes(out_slice);
+    
+        let new_slice: [u8; 8] = self.mmap[(position + OFF_WIDTH) as usize..(position + ENT_WIDTH) as usize].try_into().expect("Expected 8 bytes");
+        let new_position = u64::from_be_bytes(new_slice);
+
+        Ok((out, new_position))
 
     }
 }
