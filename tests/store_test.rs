4b8626959501e3317ad17d7d9aada0e57fc3b9f3
jscottransom <james.scottran@gmail.com>
2025-05-22 11:09:35 -0700

    feat: Fix project imports

--- tests/store_test.rs
+++ tests/store_test.rs
@@ -2,13 +2,16 @@ use tempfile::tempdir;
 use std::io::Result;
 use std::fs::File;
 
+use walrus::log::store;
+
+
 
 fn test_new_store() -> Result<()> {
     let dir = tempdir()?;
     let file_path = dir.path().join("test-new-store.txt");
     let file = File::create(file_path)?;
 
-    let store = Store::new_store(&file);
+    let store = store::Store::new_store(&file);
 
     drop(file);
     dir.close()?;
